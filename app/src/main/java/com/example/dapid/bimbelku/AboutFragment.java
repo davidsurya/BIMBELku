50cf55bd55b8698380b9694c257e08a29729c407
David Surya Aji S <dapidsurya@gmail.com>
2016-05-01 12:32:24 +0700

    second commit

--- app/src/main/java/com/example/dapid/bimbelku/AboutFragment.java
+++ app/src/main/java/com/example/dapid/bimbelku/AboutFragment.java
@@ -3,6 +3,7 @@ package com.example.dapid.bimbelku;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -22,6 +23,7 @@ public class AboutFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_about, container, false);
+        ((AppCompatActivity)getActivity()).getSupportActionBar().setTitle("Tentang");
         return v;
     }
 
