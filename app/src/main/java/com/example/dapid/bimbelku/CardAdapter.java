3e9b576a9afdc4e061f07723057651cdbe6a35dc
David Surya Aji S <dapidsurya@gmail.com>
2016-05-01 12:32:32 +0700

    second commit

--- app/src/main/java/com/example/dapid/bimbelku/CardAdapter.java
+++ app/src/main/java/com/example/dapid/bimbelku/CardAdapter.java
@@ -1,5 +1,8 @@
 package com.example.dapid.bimbelku;
 
+import android.content.ClipData;
+import android.content.Context;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
@@ -7,6 +10,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,6 +21,7 @@ import java.util.List;
 public class CardAdapter extends RecyclerView.Adapter<CardAdapter.ViewHolder>{
 
     List <ListItem> items;
+    Context context;
 
     public CardAdapter(String [] name, String [] les) {
         super();
@@ -33,6 +38,7 @@ public class CardAdapter extends RecyclerView.Adapter<CardAdapter.ViewHolder>{
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_list, parent, false);
         ViewHolder viewHolder = new ViewHolder(v);
+        context = v.getContext();
         return viewHolder;
     }
 
@@ -41,6 +47,7 @@ public class CardAdapter extends RecyclerView.Adapter<CardAdapter.ViewHolder>{
         ListItem list = items.get(position);
         holder.txtName.setText(list.getName());
         holder.txtLes.setText(list.getLes());
+        holder.currentItem = items.get(position);
     }
 
     @Override
@@ -51,9 +58,17 @@ public class CardAdapter extends RecyclerView.Adapter<CardAdapter.ViewHolder>{
     public class ViewHolder extends RecyclerView.ViewHolder{
         public TextView txtName;
         public TextView txtLes;
+        public ListItem currentItem;
 
         public ViewHolder(View itemView) {
             super(itemView);
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(context.getApplicationContext(), Tentor.class);
+                    context.startActivity(intent);
+                }
+            });
             txtName = (TextView) itemView.findViewById(R.id.txtName);
             txtLes = (TextView) itemView.findViewById(R.id.txtLes);
         }
