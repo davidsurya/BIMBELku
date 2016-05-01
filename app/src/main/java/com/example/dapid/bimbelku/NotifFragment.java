fd555163748177de62cc3e1882bfdf29c55a96f1
David Surya Aji S <dapidsurya@gmail.com>
2016-05-01 12:32:37 +0700

    second commit

--- app/src/main/java/com/example/dapid/bimbelku/NotifFragment.java
+++ app/src/main/java/com/example/dapid/bimbelku/NotifFragment.java
@@ -2,6 +2,7 @@ package com.example.dapid.bimbelku;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,6 +12,7 @@ public class NotifFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
         View v = inflater.inflate(R.layout.fragment_notif, container, false);
+        ((AppCompatActivity)getActivity()).getSupportActionBar().setTitle("Pemberitahuan");
         return  v;
     }
 }
